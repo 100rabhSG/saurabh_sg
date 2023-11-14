98a023ae6b0e48963cd41ad17eb9f84f0eba18a0
Saurabh Gupta <saurabh.agrawal.1605@gmail.com>
2023-11-14 23:35:12 +0530

    added routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+
+import Home from './Components/Home/Home.js';
+import Projects from './Components/Projects/projects.js';
+import Resume from './Components/Resume/resume.js';
 
 function App() {
   return (
-    <h1>Portfolio</h1>
+    <Router>
+      <h1>Portfolio</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/resume" element={<Resume />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/Components/Resume/resume.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Resume(){
+    return(
+        <>
+            <h1>This is Resume</h1>
+        </>
+    )
+};
+
+export default Resume;
